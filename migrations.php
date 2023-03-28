27984b27e1c9bbb9191234201464a785ad72550c
Muhammad-Anas-Darwish <muhammadanasdarwish1@gmail.com>
2023-03-28 20:14:37 +0300

    Fix migration.php

--- migrations.php
+++ migrations.php
@@ -3,10 +3,11 @@
 use silvercodes\phpmvc\Application;
 
 require_once __DIR__.'/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load(); 
 
 $config = [
+    'userClass' => \app\models\User::class,
     'db' => [
         'dsn' => $_ENV['DB_DSN'],
         'user' => $_ENV['DB_USER'],
